923de1cdd4eab2b25f494a3fba5b7181bd278d3f
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2024-01-09 17:00:54 -0500

    chore: Run lint

--- scripts/fix-dist-output.js
+++ scripts/fix-dist-output.js
@@ -2,7 +2,6 @@ import { readdir, rename, rm } from "fs/promises";
 import { resolve } from "path";
 import { fileURLToPath } from "url";
 
-
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 const parentDirName = resolve(__dirname, "..");
 const distDir = resolve(parentDirName, "dist");
@@ -13,7 +12,11 @@ const configFiles = files.filter((file) => file.includes(".config."));
 
 const srcFiles = await readdir(resolve(distDir, "src"));
 
-await Promise.all(srcFiles.map((file) => rename(resolve(distDir, "src", file), resolve(distDir, file))));
+await Promise.all(
+  srcFiles.map((file) =>
+    rename(resolve(distDir, "src", file), resolve(distDir, file))
+  )
+);
 await Promise.all(configFiles.map((file) => rm(resolve(distDir, file))));
 await rm(resolve(distDir, "src"), {
   recursive: true,
